ac156e42b1b9a98255a27a8cd0aad1b573659814
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-30 10:32:50 +0800

    done

--- src/program.cpp
+++ src/program.cpp
@@ -1,12 +1,14 @@
 //#include <emscripten.h>
 #include <iostream>
 
-using namespace std;
-
 extern "C" {
 //    EMSCRIPTEN_KEEPALIVE
-    int convert(char unit,double temp) {
-        return (unit == 'f') ? (1.8 * temp) + 32 
-        : (temp - 32) / 1.8;
-    } 
+    double faren(double temp) {
+        return (1.8 * temp) + 32.0;
+    }
+
+//    EMSCRIPTEN_KEEPALIVE
+    double celci(double temp) {
+        return (temp - 32) / 1.8;
+    }
 }
